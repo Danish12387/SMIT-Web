bcf7374e7ac48798832e18b50d3e38ded04403ef
MRsabcod <sabeebr97@gmail.com>
2024-10-02 17:52:07 +0500

    updated

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -8,9 +8,9 @@ const About = () => {
     <div className="bg-gray-100 my-10">
       <section className="flex md:flex-row flex-col-reverse items-center justify-between bg-white p-16 rounded-lg md:space-x-8">
         <div className="md:w-1/2 text-center md:text-left mb-6 md:mb-0 space-y-4">
-          <h1 className="text-4xl font-bold text-blue-700 anim-left">About <span className="text-green-600">Saylani</span></h1>
+          <h1 className="text-4xl font-bold text-blue-700 anim-left">About <span className="text-green-600">SMIT</span></h1>
           <p className="text-gray-700 text-[16px] anim-left md:text-lg">
-            Saylani Welfare is dedicated to serving humanity, with a mission to break the cycle of poverty through education, healthcare, and social welfare.
+            Saylani Mass It Training is dedicated to drive change in youth, with a mission to break the cycle of poverty through education, healthcare, and social welfare.
           </p>
           <Link to="/about" className="btn btn-active bg-blue-700 text-white hover:bg-blue-600 hover:scale-105 transition anim-left">
             <button>Read More </button><FiChevronRight className="text-xl" />
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
         <div className="min-h-screen bg-gray-100 p-6 space-y-8">
 
             <section className="bg-white p-8 rounded-lg shadow-lg mb-8">
-                <h2 className="text-3xl font-bold text-blue-700 mb-4">More About Saylani Welfare</h2>
+                <h2 className="text-3xl font-bold text-blue-700 mb-4">More About SMIT</h2>
                 <p className="text-gray-700 text-lg mb-4">
                     Saylani Welfare International Trust was founded with the aim of serving the less privileged members of society. The organization offers various services, from education to medical facilities, and works on initiatives to empower individuals through skills development.
                 </p>
